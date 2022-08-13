24bbd4fd2d255f41f7227d17b4a20579049b4604
TurboHz <turbohz@gmail.com>
2022-08-13 18:09:28 +0200

    Allows ? and ! characters in identifiers
    Had to change all foo!=bar like comparisons.
    
    Crossing fingers there are no more nasty/subtle side effects.

--- modules/gdscript/gdscript_tokenizer.cpp
+++ modules/gdscript/gdscript_tokenizer.cpp
@@ -452,7 +452,7 @@ StringName GDScriptTokenizer::get_token_literal(int p_offset) const {
 }
 
 static bool _is_text_char(CharType c) {
-	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || c == '_';
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || c == '_' || c == '?' || c == '!';
 }
 
 static bool _is_number(CharType c) {
